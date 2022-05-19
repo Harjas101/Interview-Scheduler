215a86c9f9e69237906948694d9c0a4d86f7cb11
Harjas101 <anand.harjas@gmail.com>
2022-05-19 18:05:59 -0400

    added monday tuesday and wednesday to the sidebar of the application

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,17 +1,41 @@
 import React from "react";
-
+import DayList from "./dayList";
 import "components/Application.scss";
+import { useState } from "react";
+
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
 
 export default function Application(props) {
   return (
-    
     <main className="layout">
    
+ 
       <section className="sidebar">
+        
       <img
   className="sidebar--centered"
   src="images/logo.png"
   alt="Interview Scheduler"
+/><DayList
+  days={days}
+  day={"Monday"}
+  setDay={day => console.log(day)}
 />
 <hr className="sidebar__separator sidebar--centered" />
 <nav className="sidebar__menu"></nav>
@@ -22,8 +46,10 @@ export default function Application(props) {
 />
       </section>
       <section className="schedule">
+        
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
       </section>
+      
     </main>
 );
 }
