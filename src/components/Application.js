ddca2da8ceac90cd2db0450465db8fdcff9e59f6
Harjas101 <anand.harjas@gmail.com>
2022-05-23 12:21:18 -0400

    changed prop names to use onChange and Value

--- src/components/Application.js
+++ src/components/Application.js
@@ -38,8 +38,8 @@ export default function Application(props) {
 <DayList
 
   days={days}
-  day={day}
-  setDay={setDay}
+  day={value}
+  setDay={onChange}
 />
 <hr className="sidebar__separator sidebar--centered" />
 <nav className="sidebar__menu"></nav>
