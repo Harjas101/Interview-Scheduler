af5925c41b4d6bbbd0b3c6ccb1aa905f89a8e45a
Harjas101 <anand.harjas@gmail.com>
2022-07-17 13:16:22 -0400

    add proxy

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -11,9 +11,9 @@ const useApplicationData = () => {
   });
   useEffect(() => {
     Promise.all([
-      axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments"),
-      axios.get("http://localhost:8001/api/interviewers"),
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers"),
     ])
       .then((all) => {
         const [one, two, three] = all;
